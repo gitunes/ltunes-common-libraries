32da5fb05fa928d2c7fc1e8ff0133b235cef0442
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-08-13 15:29:40 +0300

    Add api version library

---
+++ src/libs/apiversion/DogusOto.Lib.ApiVersion/Infrastructure/Constants/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace LTunes.Lib.ApiVersion.Infrastructure.Constants
+{
+    internal static class ExceptionMessage
+    {
+        internal const string ApiVersionSettingRequired = "Konfigurasyon dosyasına api versiyon ayarlarınızı ekleyiniz veya geçerli değerler tanımlayınız.";
+        internal const string ApiVersionNotCompatible = "İsteğe ait api versiyonu uyumlu değildir.";
+    }
+}
